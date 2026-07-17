08e9fe147d8b3fd1986f097fb69da979e9e37c86
NekhleshSinghSajwan <nekhleshsingh@rapidinnovation.dev>
2026-07-17 10:48:56 +0530

    jenkins file added

--- server.js
+++ server.js
@@ -8,15 +8,22 @@ app.use(express.json());
 
 app.use('/notes', noteRoutes);
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Hello from v3.0.0');
+});
+
+// 404 Handler
 app.use((req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
 
+// Error Handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Internal server error' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Notes API running on http://localhost:${PORT}`);
+    console.log(`Notes API is running on http://localhost:${PORT}`);
 });
